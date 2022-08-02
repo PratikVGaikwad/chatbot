7fd3e70f727ff5abe92a5136e10a4b0f0cd94a0a
Pratik Gaikwad <40199524+PratikVGaikwad@users.noreply.github.com>
2022-08-02 11:51:43 +0530

    Create chatbot.sh

---
+++ chatbot.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#Author: h4k3rpr4tik
+#title: chatbot
+echo
+echo "         **                  **   **                 **"  
+echo "        /**                 /**  /**                /** " 
+echo "  ***** /**       ******   ******/**       ******  ******"
+echo " **///**/******  //////** ///**/ /******  **////**///**/" 
+echo " /**  // /**///**  *******   /**  /**///**/**   /**  /**"  
+echo " /**   **/**  /** **/  /**   /**  /**  /**/**   /**  /**"  
+echo " //***** /**  /**//********  //** /****** //******   //**" 
+echo "  /////  //   //  ////////    //  /////    //////     //"  
+echo
+echo " You are chatting with Tinder Bot"
+read -p "Hello, TinderBot this side, What is your name?" name
+echo
+echo " Hi, Nice to meet you $name"
+echo
+read -p "Single?(y or n)" r1
+
+if [ $r1 = y ]
+   then 
+	echo "That's nice! kindly DM me on my Instagram profile @xyz"
+   else
+	echo "Huh, will find someone else, Thanks!"
+fi
+read -p "BTW, Where do you live?" place
+echo
+echo "$place is a very nice place to visit"
+echo
+read -p "Would you like to sing a song?(y or n)" song
+
+if [ $song = y ]
+   then 
+	echo "cool, Lets collaborate on starmaker"
+	echo "connect with this link https//testcom/"
+   else
+	echo "Goodbye! then"
+fi
